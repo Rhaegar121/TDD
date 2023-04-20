dad2634d7a8761cd5880152949c0da3473ef7fee
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-04-20 18:22:29 +0630

    update factorial method to pass the test

--- solver.rb
+++ solver.rb
@@ -1,5 +1,6 @@
 class Solver
     def factorial(num)
         return 1 if num == 0
+        num * factorial(num - 1)
     end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -9,7 +9,7 @@ describe 'Solver' do
         it 'should returns 1 when the factorial of 0' do
             expect(@solver.factorial(0)).to eq 1
         end
-        if 'should returns 24 when the factorial of 4' do
+        it 'should returns 24 when the factorial of 4' do
             expect(@solver.factorial(4)).to eq 24
         end
     end
